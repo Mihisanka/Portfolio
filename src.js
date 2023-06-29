93f21110c91ce96e777c89aeba9fb69cd69c5666
Mihisanka <mihisanka1998@gmail.com>
2023-06-30 00:11:56 +0530

    internal  commit

--- src.js
+++ src.js
@@ -1,26 +1,50 @@
-let sections = document.querySelectorAll('sections ');
-let navlinks = document.querySelectorAll('header nav a');
+  /*===================================scroll section aactive link ===============================*/
+  let menuIcon = document.querySelector('#menu-icon');
+  let navbar = document.querySelector('.navbar');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let heigth = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  menuIcon.onclick = () => {
 
-        if (top >= offset && top < offset + heigth) {
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*= ' + id + ']').classList.add('active');
+          menuIcon.classList.toggle('bx-x');
+          navbar.classList.toggle('active');
+      }
+      /*===================================scroll section aactive link ===============================*/
+  let sections = document.querySelectorAll('sections ');
+  let navlinks = document.querySelectorAll('header nav a');
 
-            });
-        };
+  window.onscroll = () => {
+      sections.forEach(sec => {
+          let top = window.scrollY;
+          let offset = sec.offsetTop - 150;
+          let heigth = sec.offsetHeight;
+          let id = sec.getAttribute('id');
 
-    });
+          if (top >= offset && top < offset + heigth) {
+              navlinks.forEach(links => {
+                  links.classList.remove('active');
+                  document.querySelector('header nav a [href*= ' + id + ']').classList.add('active');
 
-    /*===================================sticky navbar===============================*/
-    let header = document.querySelector('header');
+              });
+          };
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+      });
 
-};
+      /*===================================sticky navbar===============================*/
+      let header = document.querySelector('header');
+
+      header.classList.toggle('sticky', window.scrollY > 100);
+
+      /*===================================remove tag  icon and navbar===============================*/
+      menuIcon.classList.remove('bx-x');
+      navbar.classList.remove('active');
+  };
+  /*===================================scroll reveal===============================*/
+  ScrollReveal({
+      // reset: true,
+      distance: '80px',
+      duration: 2000,
+      display: 200
+  });
+
+  ScrollReveal().reveal('.home-contact, .heading', { origin: 'top' });
+  ScrollReveal().reveal('.home-img, .services-container,portfolio-box,contact form ', { origin: 'bottom' });
+  ScrollReveal().reveal('.home-content h1,about-img', { origin: 'left' });
